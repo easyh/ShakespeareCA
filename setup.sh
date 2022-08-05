3e500a144d64c41500aa068fbe088034617f17bc
Isih97 <isabelhansen97@yahoo.de>
2022-08-05 16:30:09 +0200

    theme code

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 mkdir -p ~/.streamlit/
 echo "\
+primaryColor = "#ffffff"
+backgroundColor = "#92b39b"
+secondaryBackgroundColor = "#55695a"
+textColor = "#ffffff"
 [server]\n\
 headless = true\n\
 port = $PORT\n\
