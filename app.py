c3621f28051a11620217ffbc65137f2236a0b44b
Isih97 <isabelhansen97@yahoo.de>
2022-08-11 08:39:20 +0200

    code inspo link

--- app.py
+++ app.py
@@ -134,6 +134,7 @@ components.html(source_code, height=h * 1.1, width=w * 1.1)
 st.sidebar.markdown('''
 ## ABOUT
 Code: https://github.com/isabelhansen\n
+Code Inspiration Credits: https://github.com/gmorinan/trekviz\n 
 Digitale Sammlung: https://shakespeare.folger.edu/ \n
 Tools: [Streamlit](https://streamlit.io/), [Altair](https://altair-viz.github.io/), [Networkx](https://networkx.org/) & [Pyvis](https://pyvis.readthedocs.io/en/latest/)
 ''')
